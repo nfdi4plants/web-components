60006d4f0f6ec6d3a9e865661f0ea394535446cc
Kevin F <Freymaurer@gmx.de>
2022-02-09 12:01:40 +0100

    update import

--- src/nfdi-navbar.ts
+++ src/nfdi-navbar.ts
@@ -1,5 +1,5 @@
 import { html, css, LitElement } from 'lit'
-import { customElement, property, query } from 'lit/decorators.js'
+import { customElement, property } from 'lit/decorators.js'
 import { bulmaStyles } from './bulma-css'
 
 
