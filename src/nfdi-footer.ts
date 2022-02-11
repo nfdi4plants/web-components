de495364b034ee378f03db8adedf95d684f2aaf4
Kevin F <Freymaurer@gmx.de>
2022-02-11 15:08:27 +0100

    remove unused import and update semver

--- src/nfdi-footer.ts
+++ src/nfdi-footer.ts
@@ -1,5 +1,5 @@
 import { html, css, LitElement } from 'lit'
-import { customElement, property } from 'lit/decorators.js'
+import { customElement } from 'lit/decorators.js'
 import { bulmaStyles } from './bulma-css'
 import { mainPageBaseUrl } from './params'
 
