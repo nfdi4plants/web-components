a5fe45d4c19c411021fa3cbc3e7143ac112134f4
Kevin F <Freymaurer@gmx.de>
2022-07-12 10:33:46 +0200

    Update Kn.-Base url

--- src/params.ts
+++ src/params.ts
@@ -1,3 +1,3 @@
 export const mainPageBaseUrl = "https://nfdi4plants.org/"
-export const knowledgeBaseUrl = "https://nfdi4plants.github.io/nfdi4plants.knowledgebase/"
+export const knowledgeBaseUrl = "https://nfdi4plants.org/nfdi4plants.knowledgebase/"
 export const gitlabBaseUrl = "https://git.nfdi4plants.org/"
